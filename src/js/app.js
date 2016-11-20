93de7b68a50dc3c5cf01888359843f4ec8544b39
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-11-20 13:29:37 +0800

    [js] add vue-router

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 import 'bulma'  // css
 import 'fa'
@@ -6,7 +7,23 @@ import 'fa'
 import App from './vue/App.vue'
 
 
+Vue.use(VueRouter)
+
+const routes = [
+  // {
+  //   path: '/',
+  //   name: 'index',
+  //   component: ...,
+  // }
+]
+
+const router = new VueRouter({
+  routes,
+})
+
+
 const app = new Vue({
-  el: 'body',
+  el: '#app',
   render: h => h(App),
+  router,
 })
